0c27eb46d87a9b10e2d1a9aac85cd3fb1daf3cb5
barrystyle <barrystyle@westnet.com.au>
2023-09-15 04:18:36 +0800

    consensus: adjust block time to 10 minutes after fork

--- src/pow.cpp
+++ src/pow.cpp
@@ -33,9 +33,11 @@ unsigned int static DarkGravityWave(const CBlockIndex* pindexLast)
         return Params().ProofOfWorkLimit().GetCompact();
     }
 
+    bool passed_fork = pindexLast->nHeight + 1 >= 2565000;
+
     if (pindexLast->nHeight > Params().LAST_POW_BLOCK()) {
         uint256 bnTargetLimit = (~uint256(0) >> 24);
-        int64_t nTargetSpacing = 60;
+        int64_t nTargetSpacing = passed_fork ? 600 : 60;
         int64_t nTargetTimespan = 60 * 40;
 
         int64_t nActualSpacing = 0;
